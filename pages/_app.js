6a8b899094bdc77be2aa8fad2addfbbe0211874f
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-07-04 23:59:49 -0500

    Add title to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import RouteGuard from '../components/RouteGuard';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps, router }) {
   return <RouteGuard router={router}>
+    <Head>
+        <title>
+          La Granja
+        </title>
+    </Head>
     <Component {...pageProps} />
   </RouteGuard>
 
